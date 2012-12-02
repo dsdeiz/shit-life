c31a93473065693f1eb10f791f9d93ea09846d54
Arnold French <arnold@promethost.com>
2012-12-03 01:26:35 +0800

    Files and shit..

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function() {
+
+  var shitLife     = new Date(2013, 0, 19);
+  var current      = new Date();
+  var shitLifeText = 'I now have a shitty life. :(';
+
+  if (+shitLife != +current) {
+    $('#shitlife-counter').countdown({
+      until:        shitLife,
+      description:  '...till I get a shitty life.',
+      expiryText:   shitLifeText
+    });
+  }
+  else {
+    $('#shitlife-counter').text(shitLifeText);
+  }
+})
